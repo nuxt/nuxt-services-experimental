7c1bb0bee6457841004a2684ced54e23e4795c99
Jonas Galvez <jonasgalvez@gmail.com>
2018-12-22 08:02:08 -0200

    infer services for mongodb services

--- modules/nuxt-mongodb/index.js
+++ modules/nuxt-mongodb/index.js
@@ -1,30 +1,48 @@
 import consola from 'consola'
 import { MongoClient } from 'mongodb'
 
-export default async function (options) {
-  const mongodb = Object.assign({}, options, this.options.mongodb)
+function connect(id, settings) {
 
-  if (!mongodb.url) {
-    throw new Error('No `mongodb.url` configuration found')
+  if (!settings.url) {
+    throw new Error(`No \`url\` configuration found for \`${id}\``)
   }
 
-  let dbNameMatch = mongodb.url.match(/[^/]+\/([^/]+)$/)
+  let dbNameMatch = settings.url.match(/[^/]+\/([^/]+)$/)
 
   if (dbNameMatch) {
-    mongodb.dbName = dbNameMatch[1]
-  } else if (!mongodb.dbName) {
+    settings.dbName = dbNameMatch[1]
+  } else if (!settings.dbName) {
     // If dbName is neither present in url nor separately defined
-    throw new Error('No `mongodb.dbName` configuration found')
+    throw new Error('No `settings.dbName` configuration found')
   }
 
   // Defaults
-  mongodb.findLimitDefault = mongodb.findLimitDefault || 20
-  mongodb.findLimitMax = mongodb.findLimitMax || 100
+  settings.findLimitDefault = settings.findLimitDefault || 20
+  settings.findLimitMax = settings.findLimitMax || 100
 
-  consola.info(`Connecting to ${mongodb.url}...`)
-  const client = await MongoClient.connect(mongodb.url, { useNewUrlParser: true, ...mongodb.options })
-  const db = client.db(mongodb.dbName)
-  consola.info(`Connected to ${mongodb.dbName} database`)
+  consola.info(`Connecting to ${settings.url}...`)
+  const client = await MongoClient.connect(settings.url, {
+    useNewUrlParser: true,
+    ...settings.options
+  })
+  const db = client.db(settings.dbName)
+  consola.info(`Connected to ${settings.dbName} database`)
 
-  this.nuxt.$db = db
+  return db
+}
+
+export default async function (options) {
+  for (const service in this.options.services) {
+    const serviceData = this.options.services[service]
+    if (Array.isArray(serviceData) && serviceData[0] === 'mongodb') {
+      let settings = {}
+      if (!serviceData[1]) {
+        serviceData[1] = {}
+      }
+      if (Object.keys(serviceData).length === 0) {
+        throw new Error(`Configuration for \`${service}\` missing.`)
+      }
+      this.nuxt[`$${service}`] = connect(...serviceData)
+    }
+  }
 }
