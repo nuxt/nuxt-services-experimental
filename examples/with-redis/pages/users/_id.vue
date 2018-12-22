c95398ebaac8ade7656782f87c56924eb5dddde5
Jonas Galvez <jonasgalvez@gmail.com>
2018-12-22 20:20:22 -0200

    add redis example

--- examples/with-redis/pages/users/_id.vue
+++ examples/with-redis/pages/users/_id.vue
@@ -8,8 +8,12 @@
 <script>
 export default {
   async asyncData({ params, $services }) {
-    const user = await $services.users.get(params.id)
-
+    let user = await $services.cache.getJSON(`user:${params.id}`)
+    if (!user) {
+      user = await $services.users.get(params.id)
+      // Cache user for 60 seconds
+      await $services.cache.setJSON(`user:${params.id}`, user, 60)
+    }
     return { user }
   }
 }
